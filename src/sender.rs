76d8f7d62814c5cd5c83a4dbeedfdb6d5f046937
Paul-Christian Volkmer <code@pcvolkmer.de>
2026-01-07 11:17:58 +0100

    feat!: add the request method to record header

--- src/sender.rs
+++ src/sender.rs
@@ -2,6 +2,7 @@ use async_trait::async_trait;
 use mv64e_mtb_dto::Mtb;
 use rdkafka::message::{Header, OwnedHeaders};
 use rdkafka::producer::{FutureProducer, FutureRecord};
+use std::fmt::{Debug, Display, Formatter};
 use std::sync::Arc;
 use std::time::Duration;
 use uuid::Uuid;
@@ -13,10 +14,25 @@ use crate::RecordKey;
 
 pub type DynMtbFileSender = Arc<dyn MtbFileSender + Send + Sync>;
 
+#[derive(PartialEq, Debug)]
+pub enum RequestMethod {
+    Post,
+    Delete,
+}
+
+impl Display for RequestMethod {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            RequestMethod::Post => write!(f, "POST"),
+            RequestMethod::Delete => write!(f, "DELETE"),
+        }
+    }
+}
+
 #[cfg_attr(test, automock)]
 #[async_trait]
 pub trait MtbFileSender {
-    async fn send(&self, mtb: Mtb) -> Result<String, ()>;
+    async fn send(&self, mtb: Mtb, method: RequestMethod) -> Result<String, ()>;
 }
 
 #[allow(clippy::module_name_repetitions)]
@@ -37,7 +53,7 @@ impl DefaultMtbFileSender {
 
 #[async_trait]
 impl MtbFileSender for DefaultMtbFileSender {
-    async fn send(&self, mtb: Mtb) -> Result<String, ()> {
+    async fn send(&self, mtb: Mtb, method: RequestMethod) -> Result<String, ()> {
         let request_id = Uuid::new_v4();
 
         let record_key = RecordKey {
@@ -48,6 +64,10 @@ impl MtbFileSender for DefaultMtbFileSender {
             .insert(Header {
                 key: "requestId",
                 value: Some(&request_id.to_string()),
+            })
+            .insert(Header {
+                key: "requestMethod",
+                value: Some(&method.to_string()),
             })
             .insert(Header {
                 key: "contentType",
@@ -75,3 +95,5 @@ impl MtbFileSender for DefaultMtbFileSender {
         }
     }
 }
+
+impl DefaultMtbFileSender {}
